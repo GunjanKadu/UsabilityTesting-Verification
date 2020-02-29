06769efb9dd33d9b666289705d01d896f1b450cf
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-29 16:43:39 +0100

    Adding Redux Dev Tools Extensions

--- Coding/FrontEnd/foodasaservice/src/Redux/Store/configureStore.ts
+++ Coding/FrontEnd/foodasaservice/src/Redux/Store/configureStore.ts
@@ -1,7 +1,16 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import { Dish } from 'Redux/Reducers/dishes';
 
+const composeEnhancers =
+  (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
+
 export const ConfigureStore = () => {
-  const store = createStore(combineReducers({ dish: Dish }));
+  const store = createStore(
+    combineReducers({ dish: Dish }),
+    composeEnhancers(applyMiddleware(thunk, logger))
+  );
   return store;
 };
