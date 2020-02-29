d6c77ab8301fc0b2347c03384458e056685162a4
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-29 17:01:42 +0100

    SettingUpAxios

---
+++ Coding/FrontEnd/foodasaservice/src/axios-config.ts
@@ -0,0 +1,3 @@
+const baseURL = 'https://food-next-door-8fdc8.firebaseio.com';
+
+export default baseURL;
--- Coding/FrontEnd/foodasaservice/src/index.tsx
+++ Coding/FrontEnd/foodasaservice/src/index.tsx
@@ -10,6 +10,31 @@ import { Provider } from 'react-redux';
 //Redux Main Store
 import { ConfigureStore } from 'Redux/Store/configureStore';
 
+//Axios Interceptors
+import axios from 'axios';
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  request => {
+    console.log(request);
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
 const store = ConfigureStore();
 
 ReactDOM.render(
