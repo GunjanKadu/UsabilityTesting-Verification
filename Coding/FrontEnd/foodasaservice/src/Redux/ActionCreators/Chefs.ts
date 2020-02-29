1a2ceac197e94e4493e992bcf55347c46734d54e
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-29 18:37:37 +0100

    Creating Action Creator For chef

---
+++ Coding/FrontEnd/foodasaservice/src/Redux/ActionCreators/Chefs.ts
@@ -0,0 +1,39 @@
+import * as ActionTypes from 'Redux/Actions/actions';
+import axios, { AxiosResponse } from 'axios';
+import baseURL from 'axios-config';
+
+export const fetchTopCooks = () => dispatch => {
+  dispatch(chefsLoading(true));
+  return axios
+    .get(baseURL + '/Chefs.json')
+    .then(
+      (response: AxiosResponse) => {
+        return response;
+      },
+      error => {
+        var errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
+    .then(response => {
+      dispatch(addChefs(response.data), dispatch(chefsLoading(false)));
+    })
+    .catch(error => {
+      dispatch(chefsFailed(error.message), dispatch(chefsLoading(false)));
+    });
+};
+
+export const addChefs = chef => ({
+  type: ActionTypes.FETCH_TOP_CHEFS,
+  payload: chef
+});
+
+export const chefsFailed = errorMessage => ({
+  type: ActionTypes.TOP_CHEFS_LOADING_FAILED,
+  payload: errorMessage
+});
+
+export const chefsLoading = value => ({
+  type: ActionTypes.CHEFS_LOADING,
+  payload: value
+});
