82f0c048fc44f662dabfc2d0696031eaa72a60cc
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-29 18:41:54 +0100

    Combining Reducers

--- Coding/FrontEnd/foodasaservice/src/Redux/Store/configureStore.ts
+++ Coding/FrontEnd/foodasaservice/src/Redux/Store/configureStore.ts
@@ -3,13 +3,14 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import { Dish } from 'Redux/Reducers/dishes';
+import { Chef } from 'Redux/Reducers/chef';
 
 const composeEnhancers =
   (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
 
 export const ConfigureStore = () => {
   const store = createStore(
-    combineReducers({ dish: Dish }),
+    combineReducers({ dish: Dish, chef: Chef }),
     composeEnhancers(applyMiddleware(thunk, logger))
   );
   return store;
