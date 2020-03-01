6292f8362e56fe65847b624f3097254ac4279d0c
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-01 18:54:47 +0100

    Enabling ReduxDevTools  Trace

--- Coding/FrontEnd/foodasaservice/src/Redux/Store/configureStore.ts
+++ Coding/FrontEnd/foodasaservice/src/Redux/Store/configureStore.ts
@@ -4,13 +4,18 @@ import logger from 'redux-logger';
 
 import { Dish } from 'Redux/Reducers/dishes';
 import { Chef } from 'Redux/Reducers/chef';
-
+import { Cuisines } from 'Redux/Reducers/cuisines';
 const composeEnhancers =
-  (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
+  ((window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) &&
+    (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']({
+      trace: true,
+      traceLimit: 25
+    }) as typeof compose)) ||
+  compose;
 
 export const ConfigureStore = () => {
   const store = createStore(
-    combineReducers({ dish: Dish, chef: Chef }),
+    combineReducers({ dish: Dish, chef: Chef, cuisine: Cuisines }),
     composeEnhancers(applyMiddleware(thunk, logger))
   );
   return store;
