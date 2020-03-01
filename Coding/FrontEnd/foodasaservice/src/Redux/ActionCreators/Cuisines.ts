e6836045b0e272b7a25f467d670cf77c089389bc
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-01 19:40:51 +0100

    Adding Cuisines Action Creator

---
+++ Coding/FrontEnd/foodasaservice/src/Redux/ActionCreators/Cuisines.ts
@@ -0,0 +1,44 @@
+import * as ActionTypes from 'Redux/Actions/actions';
+import axios, { AxiosResponse } from 'axios';
+import baseURL from 'axios-config';
+
+export const fetchCuisines = () => dispatch => {
+  dispatch(cuisinesLoading(true));
+  return setTimeout(() => {
+    axios
+      .get(baseURL + '/cuisine')
+      .then(
+        (response: AxiosResponse) => {
+          return response;
+        },
+        error => {
+          var errMess = new Error(error.message);
+          throw errMess;
+        }
+      )
+      .then(response => {
+        dispatch(addCuisines(response.data), dispatch(cuisinesLoading(false)));
+      })
+      .catch(error => {
+        dispatch(
+          cuisinesFailed(error.message),
+          dispatch(cuisinesLoading(false))
+        );
+      });
+  }, 1500);
+};
+
+export const addCuisines = cuisines => ({
+  type: ActionTypes.FETCH_CUISINES,
+  payload: cuisines
+});
+
+export const cuisinesFailed = errorMessage => ({
+  type: ActionTypes.CUISINES_LOADING_FAILED,
+  payload: errorMessage
+});
+
+export const cuisinesLoading = value => ({
+  type: ActionTypes.CUISINES_LOADING,
+  payload: value
+});
