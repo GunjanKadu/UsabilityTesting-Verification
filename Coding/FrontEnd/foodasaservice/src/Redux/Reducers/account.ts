9160592b1f894d25f7ab0959f5c1717d0ac60116
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-05 20:03:33 +0100

    Actions And Reducer for signup

--- Coding/FrontEnd/foodasaservice/src/Redux/Reducers/account.ts
+++ Coding/FrontEnd/foodasaservice/src/Redux/Reducers/account.ts
@@ -1,10 +1,17 @@
 import * as ActionTypes from 'Redux/Actions/actions';
 
 export const Account = (
-  state = { LoginDetails: {}, isValidated: false },
+  state = {
+    Loading: false,
+    LoginDetails: {},
+    isValidated: false,
+    isSuccessfullyAdded: false,
+    signUpError: ''
+  },
   action
 ) => {
   switch (action.type) {
+    //LOGIN
     case ActionTypes.POST_LOGIN_RESPONSE:
       return { ...state, LoginDetails: action.payload };
 
@@ -17,7 +24,24 @@ export const Account = (
         LoginDetails: { token: null },
         isValidated: action.payload
       };
+
+    //SIGNUP
+    case ActionTypes.SIGNUP_SUCCESSFULL:
+      return {
+        ...state,
+        isSuccessfullyAdded: action.payload
+      };
+
+    case ActionTypes.SIGNUP_ERROR:
+      return {
+        ...state,
+        signUpError: action.payload
+      };
     default:
       return state;
+
+    //LOADING
+    case ActionTypes.ACCOUNT_LOADING:
+      return { ...state, Loading: action.payload };
   }
 };
