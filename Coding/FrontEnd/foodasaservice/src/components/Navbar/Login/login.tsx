fa9389f13507d54573063e3ff55e9d75ac79c6d8
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-05 20:04:38 +0100

    Added Signin and Login Logout effect

--- Coding/FrontEnd/foodasaservice/src/components/Navbar/Login/login.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Navbar/Login/login.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+
 import { postLoginData } from 'Redux/ActionCreators/Login';
+import { postSignupData } from 'Redux/ActionCreators/SignUp';
 
 import {
   Modal,
@@ -13,7 +14,8 @@ import {
   Form,
   FormGroup,
   Label,
-  Input
+  Input,
+  Spinner
 } from 'reactstrap';
 
 const Login = props => {
@@ -22,16 +24,17 @@ const Login = props => {
   //Modal State
   const [modal, setModal] = useState(false);
 
+  //Login State
   const [loginEmail, setLoginEmail] = useState('');
   const [loginPassword, setLoginPassword] = useState('');
 
   //Nested Modal State
   const [nestedModal, setNestedModal] = useState(false);
 
+  //Sign Up state
   const [signupEmail, setsignupEmail] = useState('');
   const [signupPassword, setsignupPassword] = useState('');
   const [signupConfirmPassword, setsignupConfirmPassword] = useState('');
-
   const [signUpFullName, setSignUpFullName] = useState('');
 
   const [closeAll, setCloseAll] = useState(false);
@@ -42,27 +45,49 @@ const Login = props => {
     setNestedModal(!nestedModal);
     setCloseAll(false);
   };
+
   const toggleAll = () => {
     setNestedModal(!nestedModal);
     setCloseAll(true);
   };
 
-  const handleSubmit = event => {
+  const handleSignInSubmit = event => {
+    event.preventDefault();
+    props.postSignupData(signupEmail, signupPassword, signUpFullName);
+    setModal(false);
+    setNestedModal(false);
+  };
+
+  const handleLoginSubmit = event => {
     event.preventDefault();
     props.postLoginData(loginEmail, loginPassword);
   };
 
+  //Loading Effect
+  const isAccountLoading = props.account.Loading;
+  let loader;
+  if (isAccountLoading) {
+    loader = (
+      <div>
+        <Spinner size='sm' color='light' />
+      </div>
+    );
+  } else {
+    loader = buttonLabel;
+  }
+  //
+
   return (
     <div>
       <Button color='danger' onClick={toggle}>
-        {buttonLabel}
+        {loader}
       </Button>
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle} style={{ textAlign: 'center' }}>
           {title}
         </ModalHeader>
         <ModalBody>
-          <Form onSubmit={handleSubmit}>
+          <Form onSubmit={handleLoginSubmit}>
             <FormGroup row>
               <Label for='exampleEmail' sm={2}>
                 Email
@@ -101,78 +126,6 @@ const Login = props => {
               Login
             </Button>
           </Form>
-          <Modal
-            isOpen={nestedModal}
-            toggle={toggleNested}
-            onClosed={closeAll ? toggle : undefined}
-          >
-            <ModalHeader>Sign Up</ModalHeader>
-            <ModalBody>
-              {' '}
-              <Form>
-                <FormGroup row>
-                  <Label for='exampleEmail' sm={2}>
-                    Full Name
-                  </Label>
-                  <Col sm={10}>
-                    <Input
-                      type='Name'
-                      name='Name'
-                      id='Name'
-                      placeholder='Enter Your Full Name'
-                    />
-                  </Col>
-                </FormGroup>
-                <FormGroup row>
-                  <Label for='exampleEmail' sm={2}>
-                    Email
-                  </Label>
-                  <Col sm={10}>
-                    <Input
-                      type='email'
-                      name='email'
-                      id='email'
-                      placeholder='Enter Your Email'
-                    />
-                  </Col>
-                </FormGroup>
-                <FormGroup row>
-                  <Label for='examplePassword' sm={2}>
-                    Password
-                  </Label>
-                  <Col sm={10}>
-                    <Input
-                      type='password'
-                      name='password'
-                      id='Password'
-                      placeholder='Enter Your Password'
-                    />
-                  </Col>
-                </FormGroup>
-                <FormGroup row>
-                  <Label for='examplePassword' sm={2}>
-                    Confirm Password
-                  </Label>
-                  <Col sm={10}>
-                    <Input
-                      type='passwordconfirm'
-                      name='passwordconfirm'
-                      id='passwordconfirm'
-                      placeholder='Enter Your Password Again'
-                    />
-                  </Col>
-                </FormGroup>
-              </Form>
-            </ModalBody>
-            <ModalFooter>
-              <Button color='primary' onClick={toggleNested}>
-                SignUp{' '}
-              </Button>{' '}
-              <Button color='secondary' onClick={toggleAll}>
-                Cancel{' '}
-              </Button>
-            </ModalFooter>
-          </Modal>
         </ModalBody>
         <ModalFooter>
           <Button color='primary' onClick={toggleNested}>
@@ -182,6 +135,86 @@ const Login = props => {
             Cancel
           </Button>
         </ModalFooter>
+      </Modal>
+
+      {/* SignUp Modal */}
+      <Modal
+        isOpen={nestedModal}
+        toggle={toggleNested}
+        onClosed={closeAll ? toggle : undefined}
+      >
+        <ModalHeader>Sign Up</ModalHeader>
+        <ModalBody>
+          {' '}
+          <Form onSubmit={handleSignInSubmit}>
+            <FormGroup row>
+              <Label for='exampleEmail' sm={2}>
+                Full Name
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type='Name'
+                  name='Name'
+                  value={signUpFullName}
+                  onChange={event => setSignUpFullName(event.target.value)}
+                  placeholder='Enter Your Full Name'
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for='exampleEmail' sm={2}>
+                Email
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type='email'
+                  name='email'
+                  value={signupEmail}
+                  onChange={event => setsignupEmail(event.target.value)}
+                  placeholder='Enter Your Email'
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for='examplePassword' sm={2}>
+                Password
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type='password'
+                  name='password'
+                  value={signupPassword}
+                  onChange={event => setsignupPassword(event.target.value)}
+                  placeholder='Enter Your Password'
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for='examplePassword' sm={2}>
+                Confirm Password
+              </Label>
+              <Col sm={10}>
+                <Input
+                  type='password'
+                  name='passwordconfirm'
+                  value={signupConfirmPassword}
+                  onChange={event =>
+                    setsignupConfirmPassword(event.target.value)
+                  }
+                  placeholder='Enter Your Password Again'
+                />
+              </Col>
+            </FormGroup>
+            <ModalFooter>
+              <Button color='primary' onClick={toggleNested} type='submit'>
+                SignUpI{' '}
+              </Button>{' '}
+              <Button color='secondary' onClick={toggleAll}>
+                Cancel{' '}
+              </Button>
+            </ModalFooter>
+          </Form>
+        </ModalBody>
       </Modal>
     </div>
   );
@@ -195,6 +228,9 @@ const mapDispatchToProps = dispatch => {
   return {
     postLoginData: (email, password) => {
       dispatch(postLoginData(email, password));
+    },
+    postSignupData: (email, password, fullName) => {
+      dispatch(postSignupData(email, password, fullName));
     }
   };
 };
