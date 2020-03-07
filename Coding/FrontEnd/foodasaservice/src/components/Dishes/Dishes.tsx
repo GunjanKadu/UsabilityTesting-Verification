43918ffd499b8f9df7810caca1b3735588b721a1
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-07 12:53:24 +0100

    Fetching List of all Dishes

--- Coding/FrontEnd/foodasaservice/src/components/Dishes/Dishes.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Dishes/Dishes.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
 import DishList from 'components/Dishes/DishList/DishList';
 import DishDetail from 'components/Dishes/DishDetail/DishDetail';
 import SearchBar from 'components/Dishes/SearchBar/SearchBar';
-const Dish = () => {
+
+import { fetchAllDishes } from 'Redux/ActionCreators/Dishes';
+
+const Dish = props => {
+  useEffect(() => {
+    props.fetchAllDishes();
+  });
   return (
     <div style={{ position: 'relative', top: '13%' }}>
       <div className='row'>
@@ -22,5 +30,17 @@ const Dish = () => {
     </div>
   );
 };
-
-export default Dish;
+const mapStateToProps = state => {
+  return {
+    allDishes: state.AllDishes,
+    account: state.Account
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAllDishes: () => {
+      dispatch(fetchAllDishes());
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Dish);
