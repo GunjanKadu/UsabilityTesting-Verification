3c8882c997ed647878073c58ba28e5c608039b19
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-07 02:28:34 +0100

    DishhList Component

---
+++ Coding/FrontEnd/foodasaservice/src/components/Dishes/DishList/DishList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DishItem from 'components/Dishes/DishList/DishItem/DishItem';
+
+const DishList = () => {
+  return (
+    <div>
+      <DishItem />
+    </div>
+  );
+};
+
+export default DishList;
