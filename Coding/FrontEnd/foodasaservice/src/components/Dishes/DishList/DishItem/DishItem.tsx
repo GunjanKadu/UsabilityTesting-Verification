169a431b1b4b50f89115ebc24441fccc98ceb344
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-07 23:46:56 +0100

    Implementing Search Filter

--- Coding/FrontEnd/foodasaservice/src/components/Dishes/DishList/DishItem/DishItem.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Dishes/DishList/DishItem/DishItem.tsx
@@ -7,100 +7,190 @@ import { fetchAllDishes } from 'Redux/ActionCreators/Dishes';
 import like from 'assests/images/like.png';
 
 const DishItem = props => {
-  const { match, location, history } = props;
+  const { match, inputValue } = props;
 
   useEffect(() => {
     props.fetchAllDishes();
   }, []);
 
   const listOfAllDishes = props.allDishes.DishList;
+
   //Global Cuisines Array
   const cuisines = props.cuisines.Cuisine;
 
   const RenderDishesItems = () => {
     const renderDishItems = listOfAllDishes.map(item => {
-      // Cuisines Array in Each Dish
-      const cuisinesInEachDish = item.cuisine;
+      if (!inputValue) {
+        // Cuisines Array in Each Dish
+        const cuisinesInEachDish = item.cuisine;
 
-      //Mapping Cuisines of each Dish with the Global Cuisines Array
-      const cuisineOfEachDish = cuisinesInEachDish.map(element => {
-        const dishCuisine = cuisines.find(cuisineElement => {
-          return element === cuisineElement.id;
+        //Mapping Cuisines of each Dish with the Global Cuisines Array
+        const cuisineOfEachDish = cuisinesInEachDish.map(element => {
+          const dishCuisine = cuisines.find(cuisineElement => {
+            return element === cuisineElement.id;
+          });
+          //Returning Array of Mapped Global Cuisines Array with Cuisines Array  in Each Dish
+          return dishCuisine;
         });
-        //Returning Array of Mapped Global Cuisines Array with Cuisines Array  in Each Dish
-        return dishCuisine;
-      });
 
-      //Rendering Each Cuisines for Dish
-      const CuisinesNames = () => {
-        return cuisineOfEachDish.map(name => {
-          return (
-            <span key={name.id}>
-              {name.name} {''} {''}{' '}
-            </span>
-          );
-        });
-      };
+        //Rendering Each Cuisines for Dish
+        const CuisinesNames = () => {
+          return cuisineOfEachDish.map(name => {
+            return (
+              <span key={name.id}>
+                {name.name} {''} {''}{' '}
+              </span>
+            );
+          });
+        };
 
-      return (
-        <div key={item.id} style={{ cursor: 'pointer' }}>
-          <Link
-            to={`${match.url}/${item.id}`}
-            style={{ textDecoration: 'none', color: 'black' }}
-          >
-            <Card
-              body
-              style={{
-                backgroundColor: 'white',
-                marginBottom: '10px',
-                borderRadius: '4px',
-                height: '130px'
-              }}
+        return (
+          <div key={item.id} style={{ cursor: 'pointer' }}>
+            <Link
+              to={`${match.url}/${item.id}`}
+              style={{ textDecoration: 'none', color: 'black' }}
             >
-              <img
+              <Card
+                body
                 style={{
-                  position: 'absolute',
-                  left: '0px',
-                  top: '0px',
-                  width: '25%',
-                  height: '100%'
-                }}
-                src={item.img}
-              ></img>
-              <div
-                style={{
-                  width: '70%',
-                  height: '90%',
-                  position: 'relative',
-                  left: '30%'
+                  backgroundColor: 'white',
+                  marginBottom: '10px',
+                  borderRadius: '4px',
+                  height: '130px'
                 }}
               >
-                {' '}
-                <CardTitle style={{ fontWeight: 'bold' }}>
-                  <h6 style={{ color: '#dc3545' }}> {item.dish_name}</h6>
-                  <CuisinesNames />
-                  <img
-                    src={like}
-                    style={{ height: '15px', width: '15px' }}
-                    alt=''
-                  />
+                <img
+                  style={{
+                    position: 'absolute',
+                    left: '0px',
+                    top: '0px',
+                    width: '25%',
+                    height: '100%'
+                  }}
+                  src={item.img}
+                ></img>
+                <div
+                  style={{
+                    width: '70%',
+                    height: '90%',
+                    position: 'relative',
+                    left: '30%'
+                  }}
+                >
+                  {' '}
+                  <CardTitle style={{ fontWeight: 'bold' }}>
+                    <h6 style={{ color: '#dc3545' }}> {item.dish_name}</h6>
+                    <CuisinesNames />
+                    <img
+                      src={like}
+                      style={{ height: '15px', width: '15px' }}
+                      alt=''
+                    />
 
-                  <Badge
-                    href='#'
-                    style={{ backgroundColor: '#e5e5e5', color: 'black' }}
-                  >
-                    {item.likes}
-                  </Badge>
-                </CardTitle>
-                <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </CardText>
-              </div>
-            </Card>
-          </Link>
-        </div>
-      );
+                    <Badge
+                      href='#'
+                      style={{ backgroundColor: '#e5e5e5', color: 'black' }}
+                    >
+                      {item.likes}
+                    </Badge>
+                  </CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                </div>
+              </Card>
+            </Link>
+          </div>
+        );
+      }
+      if (
+        inputValue !== '' &&
+        item.dish_name.toLowerCase().indexOf(inputValue) !== -1
+      ) {
+        // Cuisines Array in Each Dish
+        const cuisinesInEachDish = item.cuisine;
+
+        //Mapping Cuisines of each Dish with the Global Cuisines Array
+        const cuisineOfEachDish = cuisinesInEachDish.map(element => {
+          const dishCuisine = cuisines.find(cuisineElement => {
+            return element === cuisineElement.id;
+          });
+          //Returning Array of Mapped Global Cuisines Array with Cuisines Array  in Each Dish
+          return dishCuisine;
+        });
+
+        //Rendering Each Cuisines for Dish
+        const CuisinesNames = () => {
+          return cuisineOfEachDish.map(name => {
+            return (
+              <span key={name.id}>
+                {name.name} {''} {''}{' '}
+              </span>
+            );
+          });
+        };
+
+        return (
+          <div key={item.id} style={{ cursor: 'pointer' }}>
+            <Link
+              to={`${match.url}/${item.id}`}
+              style={{ textDecoration: 'none', color: 'black' }}
+            >
+              <Card
+                body
+                style={{
+                  backgroundColor: 'white',
+                  marginBottom: '10px',
+                  borderRadius: '4px',
+                  height: '130px'
+                }}
+              >
+                <img
+                  style={{
+                    position: 'absolute',
+                    left: '0px',
+                    top: '0px',
+                    width: '25%',
+                    height: '100%'
+                  }}
+                  src={item.img}
+                ></img>
+                <div
+                  style={{
+                    width: '70%',
+                    height: '90%',
+                    position: 'relative',
+                    left: '30%'
+                  }}
+                >
+                  {' '}
+                  <CardTitle style={{ fontWeight: 'bold' }}>
+                    <h6 style={{ color: '#dc3545' }}> {item.dish_name}</h6>
+                    <CuisinesNames />
+                    <img
+                      src={like}
+                      style={{ height: '15px', width: '15px' }}
+                      alt=''
+                    />
+
+                    <Badge
+                      href='#'
+                      style={{ backgroundColor: '#e5e5e5', color: 'black' }}
+                    >
+                      {item.likes}
+                    </Badge>
+                  </CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                </div>
+              </Card>
+            </Link>
+          </div>
+        );
+      }
     });
     return <>{renderDishItems}</>;
   };
