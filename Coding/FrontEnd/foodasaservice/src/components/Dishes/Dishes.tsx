dbb3d436c172810f8fb073e585b13c6e63098203
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-07 02:29:15 +0100

    Dish Container

--- Coding/FrontEnd/foodasaservice/src/components/Dishes/Dishes.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Dishes/Dishes.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import DishList from 'components/Dishes/DishList/DishList';
+import DishDetail from 'components/Dishes/DishDetail/DishDetail';
+import SearchBar from 'components/Dishes/SearchBar/SearchBar';
 const Dish = () => {
   return (
-    <div>
-      <h3>Dish Component</h3>
+    <div style={{ position: 'relative', top: '13%' }}>
+      <div className='row'>
+        <div className='col-4' style={{ height: '80vh' }}>
+          <SearchBar />
+          <DishList />
+        </div>
+        <div
+          className='col-6'
+          style={{
+            height: '80vh'
+          }}
+        >
+          <DishDetail />
+        </div>
+      </div>
     </div>
   );
 };
