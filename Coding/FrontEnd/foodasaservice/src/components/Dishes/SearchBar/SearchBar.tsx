f1e4b8e8267bbee880b054c8d52fc4314cc5b02b
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-08 19:10:33 +0100

    Appending Search Bar on Dishes page

--- Coding/FrontEnd/foodasaservice/src/components/Dishes/SearchBar/SearchBar.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Dishes/SearchBar/SearchBar.tsx
@@ -16,14 +16,14 @@ const SearchBar = ({ onChangeValue, value }) => {
         <Row>
           <Col>
             <InputGroup style={{ width: '95.5%', margin: '0px 0px 2% 2%' }}>
-              <InputGroupAddon addonType='prepend'>
-                <Button color='success'>Search</Button>
-              </InputGroupAddon>
               <Input
                 placeholder='Enter Your Favourite Dish'
                 value={value}
                 onChange={onChangeValue}
               />
+              <InputGroupAddon addonType='append'>
+                <Button color='success'>Search</Button>
+              </InputGroupAddon>
             </InputGroup>
           </Col>
         </Row>
