c0c7b5fd6e409a77e9284bf78d4d8cd5bf1ae551
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-03-10 15:38:03 +0100

    Showing Cart Total Price

--- Coding/FrontEnd/foodasaservice/src/components/Cart/Account/Account.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Cart/Account/Account.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Toast, ToastBody, ToastHeader, Row, Col, Input } from 'reactstrap';
-import COD from 'assets/images/COD.png';
-import OnlinePayment from 'assets/images/OnlinePayment.png';
+import React from "react";
+import { Toast, ToastBody, ToastHeader, Row, Col, Input } from "reactstrap";
+import COD from "assets/images/COD.png";
+import OnlinePayment from "assets/images/OnlinePayment.png";
 
 const Account = () => {
   return (
-    <div style={{ position: 'relative', left: '34%' }}>
-      <h4 style={{ position: 'relative', left: '1%', width: 'fit-content' }}>
+    <div style={{ position: "relative", left: "34%" }}>
+      <h4 style={{ position: "relative", left: "1%", width: "fit-content" }}>
         Account
       </h4>
-      <Row style={{ marginBottom: '2%' }}>
+      <Row style={{ marginBottom: "2%" }}>
         <Col sm={12}>
-          <Toast>
+          <Toast style={{ border: "1px solid #c82333" }}>
             <ToastHeader>Delivery Address</ToastHeader>
             <ToastBody>
               LOREM IPSUM GENERATOR Lorem ipsum dolor sit amet, consectetur
@@ -19,51 +19,38 @@ const Account = () => {
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur{' '}
+              velit esse cillum dolore eu fugiat nulla pariatur{" "}
             </ToastBody>
           </Toast>
         </Col>
       </Row>
-      <Row style={{ marginBottom: '2%' }}>
+      <Row style={{ marginBottom: "2%" }}>
         <Col sm={12}>
-          <Toast>
+          <Toast style={{ border: "1px solid #c82333" }}>
             <ToastHeader>Payment Methods</ToastHeader>
-            <ToastBody style={{ textAlign: 'left', fontWeight: 'bold' }}>
-              <img style={{ height: '50px', width: '50px' }} src={COD} alt='' />
-              <span style={{ marginLeft: '10px' }}>Cash On Delievery</span>
+            <ToastBody style={{ textAlign: "left", fontWeight: "bold" }}>
+              <img style={{ height: "50px", width: "50px" }} src={COD} alt="" />
+              <span style={{ marginLeft: "10px" }}>Cash On Delievery</span>
               <Input
-                style={{ position: 'relative', left: '18%' }}
-                type='radio'
-                name='COD'
-                checked='checked'
+                style={{ position: "relative", left: "18%" }}
+                type="radio"
+                name="COD"
+                checked="checked"
               ></Input>
             </ToastBody>
-            <ToastBody style={{ textAlign: 'left', fontWeight: 'bold' }}>
+            <ToastBody style={{ textAlign: "left", fontWeight: "bold" }}>
               <img
-                style={{ height: '50px', width: '50px' }}
+                style={{ height: "50px", width: "50px" }}
                 src={OnlinePayment}
-                alt=''
+                alt=""
               />
-              <span style={{ marginLeft: '10px' }}>Online Payment</span>
+              <span style={{ marginLeft: "10px" }}>Online Payment</span>
               <Input
-                style={{ position: 'relative', left: '26%' }}
-                type='radio'
-                disabled='disabled'
-                name='Online Payment'
+                style={{ position: "relative", left: "26%" }}
+                type="radio"
+                disabled="disabled"
+                name="Online Payment"
               ></Input>
-            </ToastBody>
-          </Toast>
-        </Col>
-      </Row>
-      <Row style={{ marginBottom: '4%' }}>
-        <Col sm={12}>
-          <Toast>
-            <ToastBody style={{ textAlign: 'left', fontWeight: 'bold' }}>
-              <span> Promo</span>{' '}
-              <input type='text' placeholder='Enter Promo Code' />
-              <button className='btn btn-danger' style={{ marginLeft: '18px' }}>
-                Apply
-              </button>
             </ToastBody>
           </Toast>
         </Col>
--- Coding/FrontEnd/foodasaservice/src/components/Cart/Summary/Summary.tsx
+++ Coding/FrontEnd/foodasaservice/src/components/Cart/Summary/Summary.tsx
@@ -1,72 +1,116 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { Toast, ToastBody, ToastHeader, Row, Col } from 'reactstrap';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Toast, ToastBody, ToastHeader, Row, Col, Badge } from "reactstrap";
 
 const Summary = props => {
   useEffect(() => {}, [props.cart.CartContent]);
 
   const orderedItems = props.cart.CartContent;
+  const price = props.cart.Price;
 
   const RenderItems = () => {
     if (orderedItems.length > 0) {
       return orderedItems.map(item => {
         return (
-          <ToastBody key={item.id}>
-            <span style={{ fontWeight: 'bold' }}>{item.name}</span>{' '}
-            <span style={{ marginLeft: '70%' }}> € {item.price}</span>
+          <ToastBody key={item.id} style={{ position: "relative" }}>
+            <Row>
+              <Col sm={3}>
+                <span style={{ fontWeight: "bold" }}>{item.name}</span>{" "}
+              </Col>
+              <Col sm={3}>
+                <span style={{ position: "relative", left: "30%" }}>
+                  {" "}
+                  € {item.price}
+                </span>
+              </Col>{" "}
+              <Col>
+                <span style={{ position: "relative", left: "30%" }}>
+                  {" "}
+                  <Badge
+                    color="danger"
+                    style={{
+                      fontWeight: "bold",
+                      position: "relative",
+                      left: "60%",
+                      top: "-6%",
+                      cursor: "pointer"
+                    }}
+                  >
+                    X
+                  </Badge>
+                </span>
+              </Col>
+            </Row>
           </ToastBody>
         );
       });
     } else {
       return (
         <ToastBody>
-          Choose delicious dishes from the menu and order your menu.{' '}
+          Choose delicious dishes from the menu and order your menu.{" "}
         </ToastBody>
       );
     }
   };
   return (
     <div>
-      <h4 style={{ position: 'relative', left: '1%', width: 'fit-content' }}>
+      <h4 style={{ position: "relative", left: "1%", width: "fit-content" }}>
         Summary
       </h4>
-      <Row style={{ marginBottom: '2%' }}>
+      <Row style={{ marginBottom: "2%" }}>
         <Col sm={12}>
-          <Toast>
+          <Toast style={{ border: "1px solid #c82333" }}>
             <ToastHeader>Ordered Dishes</ToastHeader>
             <RenderItems />
           </Toast>
         </Col>
       </Row>
-      <Row style={{ marginBottom: '2%' }}>
+      <Row style={{ marginBottom: "2%" }}>
         <Col sm={12}>
-          <Toast>
-            <ToastBody style={{ textAlign: 'left', fontWeight: 'bold' }}>
-              SubTotal:
-            </ToastBody>
-            <ToastBody style={{ textAlign: 'left', fontWeight: 'bold' }}>
-              GrandTotal:
+          <Toast style={{ border: "1px solid #c82333" }}>
+            <ToastBody style={{ position: "relative" }}>
+              <Row>
+                <Col sm={5}>
+                  <span style={{ fontWeight: "bold" }}>SubTotal:</span>{" "}
+                </Col>
+                <Col sm={5}>
+                  <span style={{ position: "relative", left: "30%" }}>
+                    {" "}
+                    € {price}
+                  </span>
+                </Col>
+              </Row>
+              <Row>
+                <Col sm={5}>
+                  <span style={{ fontWeight: "bold" }}>GrandTotal:</span>{" "}
+                </Col>
+                <Col sm={5}>
+                  <span style={{ position: "relative", left: "30%" }}>
+                    {price == 0 ? <p>€ 0</p> : <p>€ {price + 3}</p>}
+                  </span>
+                </Col>
+              </Row>
             </ToastBody>
           </Toast>
         </Col>
       </Row>
-      <Row style={{ marginBottom: '4%' }}>
+      <Row style={{ marginBottom: "4%" }}>
         <Col sm={12}>
-          <Toast>
-            <ToastBody style={{ textAlign: 'left', fontWeight: 'bold' }}>
-              <span> Promo</span>{' '}
-              <input type='text' placeholder='Enter Promo Code' />
-              <button className='btn btn-danger' style={{ marginLeft: '18px' }}>
+          <Toast style={{ border: "1px solid #c82333" }}>
+            <ToastBody style={{ textAlign: "left", fontWeight: "bold" }}>
+              <span> Promo</span>{" "}
+              <input type="text" placeholder="Enter Promo Code" />
+              <button className="btn btn-danger" style={{ marginLeft: "18px" }}>
                 Apply
               </button>
             </ToastBody>
           </Toast>
         </Col>
       </Row>
-      <Row style={{ marginBottom: '2%' }}>
+      <Row style={{ marginBottom: "2%" }}>
         <Col sm={12}>
-          <Toast>
-            <button style={{ width: '100%' }} className='btn btn-danger'>
+          <Toast style={{ border: "1px solid #c82333" }}>
+            <button style={{ width: "100%" }} className="btn btn-danger">
               Order
             </button>
           </Toast>
